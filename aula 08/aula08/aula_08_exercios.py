5d3fd60b304c3b70ce06540932e256a981f5a053
kevyntaylor106-del <kevyntaylor106@gmail.com>
2025-08-12 21:48:42 -0300

    aula_08_exercicio2_12/08/2025

---
+++ aula 08/aula08/aula_08_exercios.py
@@ -0,0 +1,14 @@
+def soma(a, b):
+   return a + b
+
+def subtracao(a, b):
+   return a - b
+
+def main():
+   resultado_soma = soma(10, 5)
+   resultado_subtracao = subtracao(10, 5)
+   print(f"\n\n\tSoma\n\n: {resultado_soma}")
+   print(f"\n\n\tSubtração\n\n: {resultado_subtracao}")
+
+if __name__ == "__main__":
+   main()
