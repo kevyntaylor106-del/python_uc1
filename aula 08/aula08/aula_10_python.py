71f68cc13d9ce83b87eb74cd9ac185887afd72e0
kevyntaylor106-del <kevyntaylor106@gmail.com>
2025-08-21 21:52:13 -0300

    aula 10 python

---
+++ aula 08/aula08/aula_10_python.py
@@ -0,0 +1,13 @@
+matriz = [
+    [1, 2, 3],
+    [10, 15, 22],
+    [18, 20, 11]
+]
+
+print("elemento(0,0,):" ,matriz[0][0])
+print("elemento (2,1):" , matriz[2][1])
+
+for linha in matriz:
+    for elemento in linha:
+        print(elemento, end='')
+    print()
